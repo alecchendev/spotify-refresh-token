d7fdd5fd2a992d080960911c6e94cfac3eab6b78
Alec Chen <alecchendev@gmail.com>
2021-03-24 12:34:24 -0500

    Add scope checkboxes, minor adjustments to styling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,27 @@ function App() {
     scope: '',
   });
 
+  const [ scopes, setScopes ] = React.useState({
+    'ugc-image-upload': false,
+    'user-read-recently-played': false,
+    'user-top-read': false,
+    'user-read-playback-position': false,
+    'user-read-playback-state': false,
+    'user-modify-playback-state': false,
+    'user-read-currently-playing': false,
+    'app-remote-control': false,
+    'playlist-modify-public': false,
+    'playlist-modify-private': false,
+    'playlist-read-private': false,
+    'playlist-read-collaborative': false,
+    'user-follow-modify': false,
+    'user-follow-read': false,
+    'user-library-modify': false,
+    'user-library-read': false,
+    'user-read-email': false,
+    'user-read-private': false,
+  });
+
   const [ outputs, setOutputs ] = React.useState({
     filled: false,
     accessToken: '',
@@ -26,14 +47,11 @@ function App() {
     return hashParams;
   }
 
-  React.useEffect(async () => {
-    
-    const params = getHashParams();
+  React.useEffect(() => {
 
-    let data = {};
+    const callApi = async (params) => {
 
-    if (params.access_token && params.refresh_token) {
-
+      let data = {};
       try {
 
         const response = await axios({
@@ -44,7 +62,6 @@ function App() {
           },
           json: true
         });
-        console.log(response);
         data = response.data;
 
       } catch (err) {
@@ -56,19 +73,56 @@ function App() {
         filled: true,
         accessToken: params.access_token,
         refreshToken: params.refresh_token,
-        data 
+        data
       });
+    }
+    
+    const params = getHashParams();
 
+    if (params.access_token && params.refresh_token) {
+      callApi(params);
     }
   }, []);
 
+  React.useEffect(() => {
+    setInputs({
+      ...inputs,
+      scope: Object.keys(scopes).filter((scope) => scopes[scope]).join(' ')
+    });
+  }, [scopes]);
+
   const handleChange = (event) => {
-    console.log(event.target.name + ": " + event.target.value);
     setInputs({
       ...inputs,
       [event.target.name]: event.target.value,
     });
-  }
+  };
+
+  const handleCheck = (event) => {
+    const newScopes = {...scopes};
+    newScopes[event.target.name] = !scopes[event.target.name];
+    setScopes(newScopes);
+  };
+
+  const handleSelectAll = (event) => {
+    const newScopes = {...scopes};
+    for (let scope in newScopes) {
+      newScopes[scope] = event.target.checked;
+    }
+    setScopes(newScopes);
+  };
+
+  const scopeElements = Object.keys(scopes).map((scope) => {
+    return <div key={scope} style={{width: '250px', display: 'inline-block'}}>
+      <input
+        type='checkbox'
+        name={scope}
+        checked={scopes[scope]}
+        onChange={handleCheck}
+      />
+      <label style={{width: '200px', marginLeft: '10px', fontWeight: 'normal'}}>{scope}</label>
+    </div>
+  })
 
   const queryString = window.location.href.split('/').slice(0, 3).join('/')
                       + "/login?clientId=" + inputs.clientId
@@ -82,8 +136,9 @@ function App() {
     <div id='container'>
       <h1>{window.location.hostname}</h1>
       <p>If this app helps you at all, feel free to star my repository so I can claim developer fame.</p>
-      <p>Repo: <a href={repoLink} target='_blank'>{repoLink}</a></p>
+      <p>Repo: <a href={repoLink} rel="noreferrer" target='_blank'>{repoLink}</a></p>
       <p><strong>Remember to add {window.location.href.split('/').slice(0, 3).join('/') + '/callback'} as a redirect uri in your app.</strong></p>
+
       <div>
         <label>Client Id</label>
         <input
@@ -92,7 +147,9 @@ function App() {
           value={inputs.clientId}
           onChange={handleChange}
         />
+
         <br/>
+
         <label>Client Secret</label>
         <input
           type="text"
@@ -100,16 +157,21 @@ function App() {
           value={inputs.clientSecret}
           onChange={handleChange}
         />
+
         <br/>
+
         <label>Scope</label>
-        <input
-          type="text"
-          name="scope"
-          value={inputs.scope}
-          onChange={handleChange}
-        />
-        <p>For multiple scopes, separate with spaces, e.g. user-top-read user-read-recently-played</p>
         <br/>
+
+        {scopeElements}
+        <input
+          type="checkbox"
+          onChange={handleSelectAll}
+        />
+        <label style={{width: '200px', marginLeft: '10px', fontWeight: 'normal'}}>Select all</label>
+
+        <br/>
+
         <a style={{color: 'black'}} href={queryString}><button >Submit</button></a>
       </div>
 
